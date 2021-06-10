ce84793ec50eb7b758dfe91f900263459125e82d
Kausik N <nkausik1999@gmail.com>
2021-06-10 14:42:41 +0530

    Update ShufflingEffects.py

--- EffectsLibrary/ShufflingEffects.py
+++ EffectsLibrary/ShufflingEffects.py
@@ -9,7 +9,7 @@ import numpy as np
 # Main Functions
 def ImageEffect_RandomShufflePixels(I):
     I_flat = np.reshape(I, (I.shape[0]*I.shape[1], I.shape[2]))
-    I_shuffled_flat = I_flat
+    I_shuffled_flat = np.copy(I_flat)
     np.random.shuffle(I_shuffled_flat)
     I_shuffled = np.reshape(I_shuffled_flat, (I.shape[0], I.shape[1], I.shape[2]))
     return I_shuffled
